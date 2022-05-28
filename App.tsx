cd97eaffb9c4428b01de325e32abd6cd7e9dfbbe
Daniel Mota <danixmotaa7x@gmail.com>
2022-05-28 14:44:03 -0300

    first Commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+// import {StatusBar} from 'expo-status-bar';
+import {View} from 'react-native';
+import React from 'react';
+import AllMusics from './src/screens/AllMusics';
+
+const App =() => {
+  return (
+    <View>
+      <AllMusics/>
+    </View>
+  );
+};
+
+
+export default App;
---
+++ src/screens/AllMusics/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const AllMusics = () => {
+  return (
+    <View style={{backgroundColor: 'yellow'}}>
+
+    </View>
+  );
+};
+
+export default AllMusics;
